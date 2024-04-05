61c10c97afa2e26e02a50e10671edaa4448647e4
isaiah <isaiahmartin847@gmail.com>
2024-04-05 13:59:54 -0600

    added the hover to the buttons

---
+++ src/app/components/about_me.tsx
@@ -0,0 +1,9 @@
+
+
+export default function AboutMe () {
+    return (
+        <>
+            <div>about me</div>
+        </>
+    )
+}
--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -3,15 +3,16 @@
 export default function NavBar() {
     return (
         <div className="bg-blue-500 flex justify-between text-white min-h-14">
-            <div className="ml-6 content-center">
+            <div className="ml-6 content-center w-1/3 text-xl font-bold">
                 <h1>Isaiahmartin.dev</h1>
             </div>
+            <div className="flex w-1/3  justify-center text-xl font-bold">
             <h1 className="content-center">Welcome</h1>
-            <div className="content-center mr-6">  
-                <button>button1</button>
-                <button>button2</button>
-                <button>button3</button>
-                <button>button4</button>
+            </div>
+            <div className="flex justify-end mr-6 w-1/3 ">  
+                <button className="bg-blue-600 p-2 m-2 rounded hover:bg-blue-300">button1</button>
+                <button className="bg-blue-600 p-2 m-2 rounded hover:bg-blue-300">button2</button>
+                <button className="bg-blue-600 p-2 m-2 rounded hover:bg-blue-300">button3</button>
             </div>
         </div>
     );
