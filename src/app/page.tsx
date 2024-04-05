3af0809413a20cd8d8504a50f60430b27e8c0f01
isaiah <isaiahmartin847@gmail.com>
2024-04-05 14:14:21 -0600

    added the button shows

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import AboutMe from "./components/about_me";
 import NavBar from "./components/nav";
 
 export default function Home() {
   return (
     <>
       <NavBar></NavBar>
+      <AboutMe></AboutMe>
     </>
   );
 }
