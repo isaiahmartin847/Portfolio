08ace58ce3af619db493833bb1078597efc528b4
isaiah <isaiahmartin847@gmail.com>
2024-04-05 12:41:33 -0600

    added the nave bar component

---
+++ src/app/components/nav.tsx
@@ -0,0 +1,17 @@
+
+
+export default function NavBar() {
+    return (
+        <div>
+            <div>
+                <h1>Isaiah</h1>
+            </div>
+            <div>
+                <button>button1</button>
+                <button>button1</button>
+                <button>button1</button>
+                <button>button1</button>
+            </div>
+        </div>
+    );
+}
