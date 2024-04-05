3fefbdedb54f8217937d0857ca71146da2987823
isaiah <isaiahmartin847@gmail.com>
2024-04-05 13:03:10 -0600

    added the stylling to the nav bar

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -2,15 +2,16 @@
 
 export default function NavBar() {
     return (
-        <div>
-            <div>
-                <h1>Isaiah</h1>
+        <div className="bg-blue-500 flex justify-between text-white min-h-14">
+            <div className="ml-6 content-center">
+                <h1>Isaiahmartin.dev</h1>
             </div>
-            <div>
+            <h1 className="content-center">Welcome</h1>
+            <div className="content-center mr-6">  
                 <button>button1</button>
-                <button>button1</button>
-                <button>button1</button>
-                <button>button1</button>
+                <button>button2</button>
+                <button>button3</button>
+                <button>button4</button>
             </div>
         </div>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import NavBar from "./components/nav";
 export default function Home() {
   return (
     <>
-      <h1>this is my person web</h1>
       <NavBar></NavBar>
     </>
   );
