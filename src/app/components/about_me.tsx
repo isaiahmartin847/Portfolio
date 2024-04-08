ff40bb486022418ac7e9627c144745d29bfb613b
isaiah <isaiahmartin847@gmail.com>
2024-04-08 16:04:26 -0600

    added the right headshot photo

--- src/app/components/about_me.tsx
+++ src/app/components/about_me.tsx
@@ -1,4 +1,5 @@
-import profile from 'public/images/profile.JPG'
+import React from 'react'
+
 
 
 
@@ -8,8 +9,7 @@ export default function AboutMe () {
             <div>
                 {/* photo div */}
                 <div>
-                    <img src={profile}></img>
-                    <img src="https://media.licdn.com/dms/image/D5635AQFdO1x1X7QyJg/profile-framedphoto-shrink_200_200/0/1707879040446?e=1713214800&v=beta&t=FV4OAzvW4Sv4SmV0D4rNDFGzTmfpkLxreqKfNB8XvCU" alt="Picture of me"></img>
+                    <img className="img"src='/headshot.png' alt='profile img' />
                 </div>
                 {/* about card */}
                 <div>
