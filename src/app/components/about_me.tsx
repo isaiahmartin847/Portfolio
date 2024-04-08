5d2897d30e0a750e216b3d9d529e264895b4457a
isaiah <isaiahmartin847@gmail.com>
2024-04-08 14:19:00 -0600

    trying to display the img

--- src/app/components/about_me.tsx
+++ src/app/components/about_me.tsx
@@ -1,9 +1,21 @@
+import profile from 'public/images/profile.JPG'
+
 
 
 export default function AboutMe () {
     return (
         <>
-            <div>about me</div>
+            <div>
+                {/* photo div */}
+                <div>
+                    <img src={profile}></img>
+                    <img src="https://media.licdn.com/dms/image/D5635AQFdO1x1X7QyJg/profile-framedphoto-shrink_200_200/0/1707879040446?e=1713214800&v=beta&t=FV4OAzvW4Sv4SmV0D4rNDFGzTmfpkLxreqKfNB8XvCU" alt="Picture of me"></img>
+                </div>
+                {/* about card */}
+                <div>
+
+                </div>
+            </div>
         </>
     )
 }
