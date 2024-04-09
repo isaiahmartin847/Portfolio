8bc7cc3d286f344f26d44dc8b367d0d70f625d07
isaiah <isaiahmartin847@gmail.com>
2024-04-09 12:52:31 -0600

    added the styling and names to the stack

--- src/app/components/about_me.tsx
+++ src/app/components/about_me.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { SiNextdotjs, SiTailwindcss } from "react-icons/si";
 import { BiLogoTypescript } from "react-icons/bi";
 import { RiJavascriptFill } from "react-icons/ri";
-import { FaReact, FaPython, FaHtml5, FaNodeJs, FaCss3Alt } from "react-icons/fa";
+import { FaReact, FaPython, FaHtml5, FaNodeJs, FaCss3Alt, FaStackOverflow } from "react-icons/fa";
 
 
 
@@ -22,34 +22,44 @@ export default function AboutMe () {
                     <div className=' w-fit ml-28 pr-10 mt-10'>
                         <p className='text-xl'> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius pariatur in numquam atque animi minima temporibus quas voluptas inventore reprehenderit esse asperiores consectetur, voluptatibus ut, quod quia beatae deleniti molestiae similique obcaecati nisi! Suscipit impedit beatae aut. Deleniti, incidunt rerum! Quidem rem voluptate, atque illum laudantium animi doloribus quibusdam placeat fuga alias. Voluptate dolore perferendis suscipit quod voluptatibus recusandae explicabo eum id ullam iste omnis ut distinctio eligendi neque magnam doloremque rerum, quo itaque nulla nisi inventore. Odit adipisci doloremque incidunt ipsa tempore, id soluta voluptas. Nam qui possimus facilis magnam saepe provident rerum quisquam excepturi, quos laboriosam temporibus debitis.</p>
                     </div>  
-                    <div className='border-black border flex'>
-                        <div>
-                            <FaReact size='5rem'/>
+                    <h1 className='ml-[5vw]'>Tech Stack</h1>
+                    <div className='border-black border flex w-fit rounded-full ml-[2vw]'>
+                        <div className='pl-6'>
+                            <FaReact size='5rem' color='#087ea4'/>
+                            <p className='text-center font-serif'>React</p>
                             
                         </div>
-                        <div>
-                            <BiLogoTypescript size='5rem'/>
+                        <div className='p-1'> 
+                            <BiLogoTypescript size='5rem' color='#3178c6'/>
+                            <p className='text-center font-serif '>Type Script</p>
                         </div>
-                        <div>
-                            <RiJavascriptFill size='5rem'/>
+                        <div className='p-1'>
+                            <RiJavascriptFill size='5rem' color='#f0db4f'/>
+                            <p className='text-center font-serif '>Java Script</p>
                         </div>
-                        <div>
+                        <div className='p-1'>
                             <SiNextdotjs size='5rem' color='#000000'/>
+                            <p className='text-center font-serif '>Next.js</p>
                         </div>
-                        <div>
-                            <FaPython size="5rem" />
+                        <div className='p-1'>
+                            <FaPython size="5rem" color='#fbcb24'/>
+                            <p className='text-center font-serif '>Python</p>
                         </div>
-                        <div>
-                            <FaHtml5 size='5rem' />
+                        <div className='p-1'>
+                            <FaHtml5 size='5rem' color='#f06529'/>
+                            <p className='text-center font-serif '>HTML</p>
                         </div>
-                        <div>
-                            <SiTailwindcss  size='5rem' />
+                        <div className='p-1'>
+                            <SiTailwindcss  size='5rem' color='#3490dc'/>
+                            <p className='text-center font-serif '>TailWindCss</p>
                         </div>
-                        <div>
-                            <FaNodeJs size='5rem' />
+                        <div className='p-1'>
+                            <FaNodeJs size='5rem' color='#68A063'/>
+                            <p className='text-center font-serif '>Node.js</p>
                         </div>
-                        <div>
-                            <FaCss3Alt size='5rem' />
+                        <div className='p-1 pr-6'>
+                            <FaCss3Alt size='5rem' color='#264de4'/>
+                            <p className='text-center font-serif '>CSS</p>
                         </div>
                         
                     </div>   
