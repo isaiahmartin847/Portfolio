2325ca422caa21e05bd1b9186b5bc0736406f843
isaiahmartin <isaiahmartin847@gmail.com>
2024-04-12 12:11:59 -0600

    added the skills component

--- client/src/app/components/project-skills.tsx
+++ client/src/app/components/project-skills.tsx
@@ -6,3 +6,21 @@ interface Props{
 
 
 
+const ProjectSkills: React.FC<Props> = ({skills}) => {
+    return (
+        <div className="w-1/4">
+            <h1 className="text-center text-lg font-bold">tech stack</h1>
+            <ul className="list-disc mt-4">
+                {skills.map((item: string) => {
+                    return (
+                        <li className="ml-7">{item}</li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
+}
+
+
+
+export default ProjectSkills
--- client/src/app/components/projects.tsx
+++ client/src/app/components/projects.tsx
@@ -1,7 +1,9 @@
 import AboutProject from "./project-description"
+import ProjectSkills from "./project-skills"
 
 export default function MyProjects () {
     const input: string = "loerm alskdjfalksdjf ;klajdshf hwae kjfhawoidf akwj fhqe 9fsadhf kajwehrf uhqf"
+    const skills: Array<string> = ["React.js", "Next.js", "Tailwind"]
 
     return (
         <div className="flex border-4 ml-[10vw] mr-[10vw] mt-16">
@@ -9,18 +11,7 @@ export default function MyProjects () {
                 <img className="p-3 w-[300px] rounded-[30px]" src="https://images.pexels.com/photos/2893685/pexels-photo-2893685.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
             </div>
             <AboutProject title="hello" description={input}/> 
-            <div className="w-1/4">
-                <h1 className="text-center text-lg font-bold">tech stack</h1>
-                <ul className="list-disc mt-4">
-                    <li className="ml-7">React.js</li>
-                    <li className="ml-7">Java script</li>
-                    <li className="ml-7">Next.js</li>
-                    <li className="ml-7">Express</li>
-                    <li className="ml-7">Tailwind</li>
-                    <li className="ml-7">Docker</li>
-                    
-                </ul>
-            </div>
+            <ProjectSkills skills={skills} />
             
         </div>
     )
