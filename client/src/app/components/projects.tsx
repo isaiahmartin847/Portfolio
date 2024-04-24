bd9300972d82ad5ab525cbe3e92f257e4f708d56
isaiah <isaiahmartin847@gmail.com>
2024-04-24 17:13:41 -0600

    added the border around the github thing

--- client/src/app/components/projects.tsx
+++ client/src/app/components/projects.tsx
@@ -15,7 +15,7 @@ export default function MyProjects () {
             <div className="w-fit relative">
                 <h1 className="font-bold text-xl text-center">Farm Website</h1>
                 <p className="text-center w-fit pr-[10%] pl-[10%] mt-4">The Covenant Gardens Farm website was created to offer shoppers an alternative way to select products without relying on physical pamphlets, saving the farm hundreds of dollars on printing and folding costs. I began by developing the front end of the application using the latest frontend technologies, including React, Next.js, JavaScript, Tailwind, and CSS. Once the front end was completed, I proceeded to implement the backend using Node.js and Express for the REST API. After finalizing the routing, I integrated it with a relational database called MySQL to manage products, users, and users cart. Additionally, I utilized version control using GitHub to maintain high code standards and for project management purposes.</p>
-                <div className="text-center bg-[#555C61] rounded absolute inset-x-0 bottom-4 text-white text-md">
+                <div className="font-bold text-center bg-[#555C61] rounded absolute inset-x-0 bottom-4 text-white text-md border-2 border-black">
                     <GithubLastUpdate />
                 </div>
             </div>
