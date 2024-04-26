ee2c6ab9cb33a57ae07d8f9bedcab84a1ba4fab0
isaiah <isaiahmartin847@gmail.com>
2024-04-25 21:09:20 -0600

    added the second project div

---
+++ client/src/app/components/personal-project/personal-title.tsx
@@ -0,0 +1,19 @@
+import GithubLastUpdate from "../github_last_update";
+
+
+
+const PersonalTitleAndDescription = () => {
+    return (
+        <div className="w-fit relative">
+            <h1 className="font-bold text-xl text-center">Farm Website</h1>
+            <p className="text-center w-fit pr-[10%] pl-[10%] mt-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias mollitia sed non impedit eius autem quo rem corrupti nobis earum, ea, officia nostrum nemo quis necessitatibus asperiores qui ex eveniet labore aspernatur laboriosam quae. Porro vel unde expedita optio qui doloremque quo ipsam blanditiis, est nostrum. Velit laborum magni distinctio ad illum. Dolores, laborum. Iure quasi ratione, nulla quis sed rem, ducimus nobis deleniti dolorum, exercitationem voluptates harum? Eius, optio.</p>
+            <div className="font-bold text-center bg-[#555C61] rounded absolute inset-x-0 bottom-4 text-white text-md border-2 border-black">
+                <GithubLastUpdate />
+            </div>
+        </div>
+    )
+}
+
+
+
+export default PersonalTitleAndDescription;
--- client/src/app/components/projects.tsx
+++ client/src/app/components/projects.tsx
@@ -3,16 +3,26 @@
 import FarmImg from "./farm-project/farm-img";
 import FarmTechStack from "./farm-project/farm-stack";
 import FarmTitleAndDescription from "./farm-project/farm-title";
+import PersonalProjectImg from "./personal-project/personal-img";
+import PersonalTitleAndDescription from "./personal-project/personal-title";
 
 
 export default function MyProjects () {
     
     return (
-        <div className="flex border-2 border-black ml-[10vw] mr-[10vw] mt-16 rounded">
-                <FarmImg />
-                <FarmTitleAndDescription />
+        <>
+            <div className="flex border-2 border-black ml-[10vw] mr-[10vw] mt-16 rounded">
+                    <FarmImg />
+                    <FarmTitleAndDescription />
+                    <FarmTechStack />
+            </div>
+
+            <div className="flex border-2 border-black ml-[10vw] mr-[10vw] mt-16 rounded">
+                <PersonalProjectImg />
+                <PersonalTitleAndDescription />
                 <FarmTechStack />
-            
-        </div>
+
+            </div>
+        </>
     )
 }
