af4563bd5d4cc9e4cd9f52529aa25603aabb0ed3
isaiah <isaiahmartin847@gmail.com>
2024-05-13 20:49:47 -0600

    added the qr code ruff draft for the descriptin

--- client/src/app/components/qrcode-project/qr-title.tsx
+++ client/src/app/components/qrcode-project/qr-title.tsx
@@ -6,7 +6,7 @@ const QrCodeDescription = () => {
     return (
         <div className="w-fit relative">
             <h1 className="font-bold text-xl text-center">QR Code Genorator</h1>
-            <p className="text-center w-fit pr-[10%] pl-[10%] mt-4"> dolorem aliquid natus! Beatae, illo obcaecati? Doloribus sunt vero maxime, odio veritatis doloremque a voluptatem est consectetur voluptates, earum aperiam assumenda ut recusandae? Dolores nisi sit laudantium voluptates, veritatis harum cum atque optio quis? Optio tenetur omnis, ex nesciunt facilis nostrum nemo fuga itaque quod vero perferendis ut vel ipsa consequuntur inventore maiores aliquid error officiis deserunt impedit.</p>
+            <p className="text-center w-fit pr-[10%] pl-[10%] mt-4">My QR code generator project is a testament to basic skills. Its important to me to peeling off all the  modern frameworks and building websites with the bear minimum of technology to fully understand underneath the hood of all these frameworks. These days i feel like everyone is chasing the new framework and the next big one while this is a good thing for the industry there are a lot of people out there who don’t understand the basics the things that all the frameworks are using under the hood. </p>
             <div className="font-bold text-center bg-[#555C61] rounded absolute inset-x-0 bottom-4 text-white text-md border-2 border-black">
                 <GithubLastUpdate 
                 repo="QRCodeGenerator"
