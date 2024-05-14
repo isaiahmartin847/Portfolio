ced51daeec51ca7412fc682e1ded18692ade6244
isaiah <isaiahmartin847@gmail.com>
2024-05-14 16:42:41 -0600

    added the text for the qr code project and added the image size

--- client/src/app/components/qrcode-project/qr-img.tsx
+++ client/src/app/components/qrcode-project/qr-img.tsx
@@ -1,7 +1,7 @@
 const QrcodeProjectImg = () => {
     return (
         <div className="p-3">
-            <img className="border-2 border-black rounded-[20px] w-[650px]" src="/photos/qr-code.png"/>
+            <img className="border-2 border-black rounded-[20px] w-[650px] h-[220px]" src="/photos/qr-code.png"/>
         </div>
     )
 }
--- client/src/app/components/qrcode-project/qr-title.tsx
+++ client/src/app/components/qrcode-project/qr-title.tsx
@@ -6,7 +6,7 @@ const QrCodeDescription = () => {
     return (
         <div className="w-fit relative">
             <h1 className="font-bold text-xl text-center">QR Code Genorator</h1>
-            <p className="text-center w-fit pr-[10%] pl-[10%] mt-4">My QR code generator project is a testament to basic skills. Its important to me to peeling off all the  modern frameworks and building websites with the bear minimum of technology to fully understand underneath the hood of all these frameworks. These days i feel like everyone is chasing the new framework and the next big one while this is a good thing for the industry there are a lot of people out there who don’t understand the basics the things that all the frameworks are using under the hood. </p>
+            <p className="text-center w-fit pr-[10%] pl-[10%] mt-4">My QR code generator project is a testament to the basics. It's important to me to peel off all the modern frameworks and build websites with three tried and true technologies to fully understand what's under the hood of all these frameworks. These days, I feel like everyone is chasing the newest framework and the next big thing. While this is a good thing for the industry, there are a lot of people out there who don’t understand the basics and the underlying principles that all the frameworks are built upon.</p>
             <div className="font-bold text-center bg-[#555C61] rounded absolute inset-x-0 bottom-4 text-white text-md border-2 border-black">
                 <GithubLastUpdate 
                 repo="QRCodeGenerator"
