aca279180957e2d086ef615ec9c35346ddbea4ae
isaiah <isaiahmartin847@gmail.com>
2024-05-22 21:25:40 -0600

    added the links to the titels of the projects on the landing page

--- client/src/app/components/personal-project/personal-title.tsx
+++ client/src/app/components/personal-project/personal-title.tsx
@@ -1,11 +1,13 @@
 import GithubLastUpdate from "../github_last_update";
+import Link from "next/link";
+
 
 
 
 const PersonalTitleAndDescription = () => {
     return (
         <div className="w-fit relative">
-            <h1 className="font-bold text-xl text-center">Personal Website</h1>
+            <Link href="/project/personal"><h1 className="font-bold text-xl text-center">Personal Website</h1></Link>
             <p className="text-center w-fit pr-[10%] pl-[10%] mt-4">My portfolio project is one of my favorites. It's a website where I can show more personality and passion for coding. This website was created with cutting-edge front-end technology. I had a lot of fun with this project; for instance, I used GitHub's RESTful API to get the latest pushes on all my GitHub projects and utilized React Icons to display my skills in an intuitive way.</p>
             <div className="font-bold text-center bg-[#555C61] rounded absolute inset-x-0 bottom-4 text-white text-md border-2 border-black">
                 <GithubLastUpdate
