512c4e80e903255e770725f68cafe62e8a52b8f2
isaiahmartin <isaiahmartin847@gmail.com>
2024-06-05 16:09:24 -0600

    added the styling to be responsive

--- client/src/app/components/about_me.tsx
+++ client/src/app/components/about_me.tsx
@@ -12,15 +12,10 @@ export default function AboutMe () {
     return (
         <>
             <div className='mt-[8vh] border-2 border-slate-600 rounded-lg ml-2 mr-2 pb-6 md:mr-[10vw] md:ml-[10vw]'>                    
-                <h1 className='text-5xl p-4 ml-6'>Isaiah Martin <span className="text-blue-500">&bull;</span> Full Stack Engineer</h1>
+                <h1 className='text-5xl p-4 md:ml-6'>Isaiah Martin <span className="text-blue-500">&bull;</span> Full Stack Engineer</h1>
                 <div>
-                    <div className='w-fit ml-28 pr-10 mt-10'>
-                        <div className='flex flex-col'>
-                            <div className='flex items-center space-x-2'>
-                                <p className='text-xl'>Hi, I&apos;m Isaiah. I&apos;m a highly motivated Full Stack Engineer focusing on front-end development. I&apos;ve worked with various technologies like robotics and drones, but I have a deep passion for creating high-performing, full-stack applications. After completing Harvard&apos;s website development class, I shifted my focus to Next.js and React with TypeScript. I&apos;ve built and deployed multiple full-stack applications for local companies. Have a look around, cheers.</p>
-                                {/* <span><FaGlassCheers size={25} className=''/></span> */}
-                            </div>
-                        </div>
+                    <div className=' ml-5 mr-5 md:ml-28 md:pr-10 md:mt-10 flex flex-col'>
+                        <p className='text-xl'>Hi, I&apos;m Isaiah. I&apos;m a highly motivated Full Stack Engineer focusing on front-end development. I&apos;ve worked with various technologies like robotics and drones, but I have a deep passion for creating high-performing, full-stack applications. After completing Harvard&apos;s website development class, I shifted my focus to Next.js and React with TypeScript. I&apos;ve built and deployed multiple full-stack applications for local companies. Have a look around, cheers.</p>
                     </div> 
                     <div>
                         <h1 className='ml-[5vw] text-xl mt-5 text-blue-500 font-bold'>Tech Stack</h1>
