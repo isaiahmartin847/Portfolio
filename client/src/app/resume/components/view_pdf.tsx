2db858f4ad96ba61998f4b771f08a6ea17074b0b
isaiah <isaiahmartin847@gmail.com>
2024-06-06 21:52:37 -0600

    added the right resume

--- client/src/app/resume/components/view_pdf.tsx
+++ client/src/app/resume/components/view_pdf.tsx
@@ -1,12 +1,20 @@
 
 // import resume1 from "./public/photos/resume1.png"
+import Image from "next/image"
 
 const ShowPdf = () => {
 
 
     return (
       <div className="flex justify-center">
-        <img className="border-black border-[1px] mt-4" src="/photos/resume.png"/>
+        {/* <img className="border-black border-[1px] mt-4" src="/photos/resume-img.png"/> */}
+        <Image
+        src="/photos/resume-img.png"
+        height={700}
+        width={700}
+        alt="resume"
+        className="border-black border-2"
+        />
       </div>
     )
 }   
