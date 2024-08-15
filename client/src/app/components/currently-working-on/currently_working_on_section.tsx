bd60ae3e90c41b1c6e91d53ae6f8222b7dcf05b4
isaiah <isaiahmartin847@gmail.com>
2024-08-15 17:27:20 -0600

    changed some wording

--- client/src/app/components/currently-working-on/currently_working_on_section.tsx
+++ client/src/app/components/currently-working-on/currently_working_on_section.tsx
@@ -8,7 +8,7 @@ import CurrentlyWorkingOnTitle from "./currently_title"
 const CurrentlyWorkingOnSection = () => {
     return (
         <div className="">
-          <h1 className="text-center font-bold text-3xl mt-[10vh]">Currently working on</h1>
+          <h1 className="text-center font-bold text-3xl mt-[10vh]">Current Project</h1>
 
           <div className="flex flex-wrap border-2 border-black mt-16 rounded md:ml-[10vw] md:mr-[10vw] md:flex-none md:flex-nowrap">
                 <CurrentlyImg />
