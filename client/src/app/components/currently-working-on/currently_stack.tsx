11a1d9e88d3bb36c10b22bfbd4388afdb4ee5184
isaiah <isaiahmartin847@gmail.com>
2024-08-15 13:28:41 -0600

    added some changes

--- client/src/app/components/currently-working-on/currently_stack.tsx
+++ client/src/app/components/currently-working-on/currently_stack.tsx
@@ -1,6 +1,7 @@
 import { FaReact,  FaDocker, FaGithub, FaPython, FaAws } from "react-icons/fa";
 import { SiNextdotjs, SiTailwindcss, SiMysql, SiNodedotjs, SiVercel, SiReactquery, SiFastapi } from "react-icons/si";
 import { BiLogoTypescript } from "react-icons/bi";
+import { BsStripe } from "react-icons/bs";
 
 
 const CurrentlyTechStack = () => {
@@ -9,7 +10,7 @@ const CurrentlyTechStack = () => {
             <div className="md:w-1/4">
             <h1 className="text-center text-lg font-bold">tech stack</h1>
             <div className="ml-4 mr-4 justify-between flex flex-wrap md:flex-nowrap md:flex-col">
-                <div className="flex text-xl"><FaReact color='#087ea4' style={{marginTop: 5, marginLeft: 2, marginRight: 2}}/> react.js</div>
+                <div className="flex text-xl"><FaReact color='#087ea4' style={{marginTop: 5, marginLeft: 2, marginRight: 2}}/> React.js</div>
                 <div className="flex text-xl"><BiLogoTypescript color='#3178c6' style={{marginTop: 5, marginLeft: 2, marginRight: 2}}/>TypeScript</div>
                 <div className="flex text-xl"><FaPython color='#FFDE57' style={{marginTop: 5, marginLeft: 2, marginRight: 2}}/>Python</div>
                 <div className="flex text-xl"><SiNextdotjs color='#000000' style={{marginTop: 5, marginLeft: 2, marginRight: 2}}/> Next.js</div>
@@ -22,6 +23,7 @@ const CurrentlyTechStack = () => {
                 <div className="flex text-xl"><FaDocker color='#0db7ed' style={{marginTop: 5, marginLeft: 2, marginRight: 2}}/> Docker</div>
                 <div className="flex text-xl"><FaAws color='#ff9900' style={{marginTop: 5, marginLeft: 2, marginRight: 2}}/> AWS</div>
                 <div className="flex text-xl"><SiVercel color='#000000' style={{marginTop: 5, marginLeft: 2, marginRight: 2}}/> Vercel</div>
+                <div className="flex text-xl"><BsStripe color='#5433FF' style={{marginTop: 5, marginLeft: 2, marginRight: 2}}/> Stripe</div>
             </div>
         </div>
     )
