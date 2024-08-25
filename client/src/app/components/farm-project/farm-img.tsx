ec9915027c9922b4bc25348cfe57775e80d5a548
isaiah <isaiahmartin847@gmail.com>
2024-08-25 17:20:05 -0600

    added the new image for farm web

--- client/src/app/components/farm-project/farm-img.tsx
+++ client/src/app/components/farm-project/farm-img.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const FarmImg = () => {
     return (
-        <div className="flex items-center justify-center mt-2 md:m-2">
+        <div className="flex items-center justify-center mt-2 md:m-2 space-x-0 space-y-3 md:space-y-0 md:space-x-4 flex-wrap">
             <div className="w-fit border-black border-2 flex items-center justify-center">
                 <Image 
                 src="/photos/farmweb.png"
@@ -11,8 +11,18 @@ const FarmImg = () => {
                 height={300}
                 alt="Image of project"
                 priority={true}
-
                 />
+                
+            </div>
+            <div className="w-fit border-black border-2 flex items-center justify-center">
+                <Image 
+                src="/photos/farm-web2.png"
+                width={300}
+                height={300}
+                alt="Image of project"
+                priority={true}
+                />
+                
             </div>
         </div>
     )
