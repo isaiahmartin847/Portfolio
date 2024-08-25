dd8c6a6702db658b3f546b4067bb9fa905358b93
isaiah <isaiahmartin847@gmail.com>
2024-08-25 16:41:20 -0600

    changed the UI of the current project section

--- client/src/app/components/currently-working-on/currently_title.tsx
+++ client/src/app/components/currently-working-on/currently_title.tsx
@@ -1,17 +1,13 @@
-import Link from "next/link";
-import CountdownTimer from "./release_date";
-
-
 
 
 const CurrentlyWorkingOnTitle = () => {
     return (
-        <div className="w-fit relative">
+        <div className="w-fit relative flex flex-col justify-center text-center items-center">
             <h1 className="font-bold text-xl text-center mt-1">Dark Mounts</h1>
-            <p className="text-center w-fit mt-10">I&apos;m currently developing an e-commerce app for Dark Mounts, a company specializing in modern European mounting systems. For the front end, I chose Next.js and React, with React Query for managing data fetching and caching. FastAPI will power the backend, selected for its rapid development capabilities and built-in Swagger support. I am using MySQL as the database, which is well-suited for managing users, products, orders, and the cart in an e-commerce environment. The backend will be containerized with Docker and deployed to AWS, while Vercel will streamline the deployment of the front end.</p>
+            <p className="w-4/5 ">I&apos;m currently developing an e-commerce app for Dark Mounts, a company specializing in modern European mounting systems. For the front end, I chose Next.js and React, with React Query for managing data fetching and caching. FastAPI will power the backend, selected for its rapid development capabilities and built-in Swagger support. I am using MySQL as the database, which is well-suited for managing users, products, orders, and the cart in an e-commerce environment. The backend will be containerized with Docker and deployed to AWS, while Vercel will streamline the deployment of the front end.</p>
             
-            <div className="ml-2 mr-2 font-bold text-center bg-[#555C61] rounded text-white text-md border-2 border-black mt-7">
-                <CountdownTimer />
+            <div className="flex justify-center">
+                <div className="border-b-2 border-black h-1 w-4/5 flex justify-center"></div>
             </div>
         </div>
     )
--- client/src/app/components/currently-working-on/currently_working_on_section.tsx
+++ client/src/app/components/currently-working-on/currently_working_on_section.tsx
@@ -10,9 +10,9 @@ const CurrentlyWorkingOnSection = () => {
         <div className="">
           <h1 className="text-center font-bold text-3xl mt-[10vh]">Current Project</h1>
 
-          <div className="flex flex-wrap border-2 border-black mt-16 rounded md:ml-[10vw] md:mr-[10vw] md:flex-none md:flex-nowrap">
-                <CurrentlyImg />
+          <div className="flex border-2 flex-col border-black mt-16 rounded mx-1 md:mx-[10vw] ">
                 <CurrentlyWorkingOnTitle />
+                <CurrentlyImg />
                 <CurrentlyTechStack />
           </div>
         </div>
