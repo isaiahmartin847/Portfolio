38d19fec30d7f7934fe75fe82dc03109483d7cde
isaiah <isaiahmartin847@gmail.com>
2024-08-25 16:51:26 -0600

    updated the current project photo and added a link to the webstie

--- client/src/app/components/currently-working-on/currently_title.tsx
+++ client/src/app/components/currently-working-on/currently_title.tsx
@@ -1,9 +1,9 @@
-
+import Link from "next/link"
 
 const CurrentlyWorkingOnTitle = () => {
     return (
         <div className="w-fit relative flex flex-col justify-center text-center items-center">
-            <h1 className="font-bold text-xl text-center mt-1">Dark Mounts</h1>
+            <Link href="https://www.darkmounts.shop/" target="_blank" className="font-bold text-xl text-center mt-1">Dark Mounts</Link>
             <p className="w-4/5 ">I&apos;m currently developing an e-commerce app for Dark Mounts, a company specializing in modern European mounting systems. For the front end, I chose Next.js and React, with React Query for managing data fetching and caching. FastAPI will power the backend, selected for its rapid development capabilities and built-in Swagger support. I am using MySQL as the database, which is well-suited for managing users, products, orders, and the cart in an e-commerce environment. The backend will be containerized with Docker and deployed to AWS, while Vercel will streamline the deployment of the front end.</p>
             
             <div className="flex justify-center">
--- client/src/app/components/currently-working-on/currently_working_on_section.tsx
+++ client/src/app/components/currently-working-on/currently_working_on_section.tsx
@@ -7,7 +7,8 @@ import CurrentlyWorkingOnTitle from "./currently_title"
 
 const CurrentlyWorkingOnSection = () => {
     return (
-        <div className="">
+        <div>
+          
           <h1 className="text-center font-bold text-3xl mt-[10vh]">Current Project</h1>
 
           <div className="flex border-2 flex-col border-black mt-16 rounded mx-1 md:mx-[10vw] ">
